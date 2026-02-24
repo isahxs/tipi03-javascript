ff3dc5601d5c25e4a483619b77a2f29cee39a485
Isa <isabelly.sfernandes.senacsp.edu.br>
2026-02-24 11:37:38 -0300

    aula 24-02

---
+++ 02-concaternacaointerpolacao/script.js
@@ -0,0 +1,20 @@
+// Concatenação
+const meutexto = "Olá" + "Mundo";
+console.log(meutexto);
+
+const numeronovo = 15;
+console.log("O número é " + numeronovo);
+
+const meutexto2 = "Olá " .concat("mundo novamente");
+console.log(meutexto2);
+
+// Interpolando uma string, um n´pumero euma expressão
+const primeironome = "João";
+const idade = 30;
+console.log(`Olá, ${primeironome}, você tem ${idade} anos.`);
+
+//comparação
+console.log("Olá, " + primeironome + ", você tem " + idade + " anos.");
+
+
+
