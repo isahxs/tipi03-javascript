47a36b5d5cc85a413c373703c6409e5609cd56a0
Isa <isabelly.sfernandes.senacsp.edu.br>
2026-03-03 11:56:34 -0300

    03-03

---
+++ 09-array/script.js
@@ -0,0 +1,21 @@
+//array = vetor: armazena dados do mesmo tipo
+
+const nomes = ["João", "Maria", "José"];
+
+console.log(nomes);
+console.log(nomes[0]);
+console.log(nomes[1]);
+console.log(nomes[2]);
+console.log(nomes[3]); //essa posição não existe(undefined)
+
+//Adicionando um elemento no array
+nomes.push("Beatriz");
+console.log(nomes);
+
+//Removendo o último elemento do array
+nomes.pop();
+console.log("Beatriz foi removida " + nomes);
+
+//Alterar um elemento do array
+nomes[0] = "João Pedro";
+console.log(nomes);
