e73d37e8ebcc77aac4388eb66afcb5c058be4b1e
Isa <isabelly.sfernandes.senacsp.edu.br>
2026-03-04 11:54:53 -0300

    loops-array

--- 09-array/script.js
+++ 09-array/script.js
@@ -19,3 +19,72 @@ console.log("Beatriz foi removida " + nomes);
 //Alterar um elemento do array
 nomes[0] = "João Pedro";
 console.log(nomes);
+
+//Concatenar dois arrays
+const numeros = [6, 7, 8];
+console.log(numeros.length);
+
+const outrosNumeros = [3, 9, 2];
+const juncaoNumeros = numeros.concat(outrosNumeros);
+console.log(juncaoNumeros);
+
+//Removendo o primeiro elemento do array
+const nomesNovos =["Joaquina", "Felipe", "Plínio"];
+
+const primeiroNome = nomesNovos.shift();
+console.log(nomesNovos);
+console.log(nomesNovos[0]);
+
+//Adicionar um ou mais elementos no início do array
+const numerosTeste = [1, 2, 3];
+
+numerosTeste.unshift(0);
+console.log(numerosTeste);
+
+//Encontrar a primeira ocorrência do elemento especificado
+const frutas = ["maçã", "banana", "manga", "abacaxi"];
+
+console.log(frutas.indexOf("maçã"));
+console.log(frutas.indexOf("laranja")); //resultado é -1 pois ele não existe
+
+//Encontrar a última ocorrência do elemento especificado
+const letras = ["a", "b", "c", "a", "d"];
+
+console.log(letras.lastIndexOf("a"));
+console.log(letras.lastIndexOf("b"));
+console.log(letras.lastIndexOf("z"));
+
+//Verifica se um elemento está presente no array
+const frutasDona = ["maçã", "banana", "laranja"];
+
+const temMaca = frutasDona.includes("maçã");
+const temMelancia = frutasDona.includes("melancia");
+console.log(temMaca);
+console.log(temMelancia);
+
+//includes à partir de uma posição
+const letrasNovo = ["a", "b", "c", "d", "a"];
+const temApos2 = letrasNovo.includes("b", 2);
+console.log(temApos2);
+
+//revertendo array
+const testandoReverse = [1, 2, 3, 4, 5]
+
+testandoReverse.reverse();
+console.log(testandoReverse);
+
+const testandoSpread = ["a", "b", "c", "d", "e"];
+console.log(testandoSpread);
+
+const novaLetras = [...testandoSpread].reverse();
+console.log(novaLetras)
+
+//converte todas as letras de uma string para maiúsculas
+const fraseA = "Boas vindas!";
+const upper = fraseA.toUpperCase();
+console.log(upper);
+
+//converte todas as letras de uma string para maiúsculas
+const fraseB = "QUE LEGAL!";
+const lower = fraseB.toLowerCase();
+console.log(lower);
